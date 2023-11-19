4b66d5f556e66fdf983a6b2ed6f8975402e826bf
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-11-19 09:04:56 -0500

    Have a default search so folks know what to expect right away

---
+++ app/routes/index.ts
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+
+import type RouterService from '@ember/routing/router-service';
+
+export default class Query extends Route {
+  @service declare router: RouterService;
+
+  async beforeModel() {
+    this.router.replaceWith('query', { queryParams: { packages: '@angular/core' } });
+  }
+}

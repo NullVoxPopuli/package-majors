317fbd81adc2426af5f252a9ef665d46fd292e8f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-06-15 20:55:07 -0400

    Remove load initializers

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,6 @@
 import Application from '@ember/application';
 import { isTesting, macroCondition } from '@embroider/macros';
 
-import loadInitializers from 'ember-load-initializers';
 import { sync } from 'ember-primitives/color-scheme';
 import Resolver from 'ember-resolver';
 import config from 'package-majors/config/environment';
@@ -11,8 +10,6 @@ export default class App extends Application {
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
 }
-
-loadInitializers(App, config.modulePrefix);
 
 if (macroCondition(isTesting())) {
   // No themes in testing... yet?

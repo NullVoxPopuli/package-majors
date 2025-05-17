90c02fade1649e30d44b2cf4ccbec6bc57515a9c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-17 01:23:33 -0400

    ope

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,9 @@ const config = configs.ember();
 export default [
   ...config,
   {
+    ignore: ["public"],
+  },
+  {
     files: ["*.{cjs,js}"],
     rules: {
       "n/no-unsupported-syntax": "off",

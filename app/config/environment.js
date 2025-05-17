881059c799f74da58f5877e73937041a82e0f3ca
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-17 01:34:13 -0400

    oi

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,3 +1,22 @@
-import loadConfigFromMeta from '@embroider/config-meta-loader';
+import { getGlobalConfig } from '@embroider/macros/src/addon/runtime';
 
-export default loadConfigFromMeta('package-majors');
+const ENV = {
+  modulePrefix: 'package-majors',
+  environment: import.meta.env.DEV ? 'development' : 'production',
+  rootURL: '/',
+  locationType: 'history',
+  EmberENV: {},
+  APP: {},
+};
+
+export default ENV;
+
+export function enterTestMode() {
+  ENV.locationType = 'none';
+  ENV.APP.rootElement = '#ember-testing';
+  ENV.APP.autoboot = false;
+
+  let config = getGlobalConfig()['@embroider/macros'];
+
+  if (config) config.isTesting = true;
+}

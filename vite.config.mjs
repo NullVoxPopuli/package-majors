d1b6bd9019aa8acd3f8b7affadaa6c30b193c1a9
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-30 12:32:03 -0500

    CLeanup

--- vite.config.mjs
+++ vite.config.mjs
@@ -15,42 +15,10 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       ember(),
-      // extra plugins here
       babel({
         babelHelpers: "runtime",
         extensions,
       }),
     ],
-    build: {
-      target: "esnext",
-      minify: isProduction ? "terser" : false,
-      terserOptions: {
-        module: true,
-        toplevel: false,
-        ecma: 2022,
-        output: {
-          comments: false,
-        },
-        compress: {
-          ecma: 2022,
-          module: true,
-          passes: 6,
-          sequences: 30,
-          arguments: false,
-          keep_fargs: false,
-          toplevel: false,
-          unsafe: true,
-          hoist_funs: true,
-          conditionals: true,
-          drop_debugger: true,
-          evaluate: true,
-          reduce_vars: true,
-          side_effects: true,
-          dead_code: true,
-          defaults: true,
-          unused: true,
-        },
-      },
-    },
   };
 });
